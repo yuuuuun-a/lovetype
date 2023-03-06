a2a5776b8071fb16752f90cfcdd987793ff05ad3
yuuuuun-a <124505011+yuuuuun-a@users.noreply.github.com>
2023-03-06 19:33:03 +0900

    Update share.js

--- js/share.js
+++ js/share.js
@@ -33,12 +33,14 @@ const url = 'https://warm-crostata-a6dd43.netlify.app/';
 // }
 
 
+
     var resultImg = document.querySelector('#resultImg');
     var resultAlt = resultImg.firstElementChild.lastElementChild; // ALT 속성 지정
     const shareTitle = '십이간지 연애유형 결과';
     const shareDes = infoList[resultAlt].name;
-    const shareImage = url + 'img/image-' + resultAlt + '.png';
-    const shareURL = url + 'page.result-' + resultAlt + '.html';
+    const shareImage = url+'img/imgae-'+resultAlt+'.png';
+    const shareURL = url+'page.result-'+resultAlt+'.html';
+    
 
 
 function goKakao(){
@@ -46,11 +48,11 @@ Kakao.init("d3f0847972aa49b92bf926aad2ad4666");
 Kakao.Link.sendDefault({
     objectType: "feed",
     content: {
-        title: "십이간지 연애유형 결과",
+        title: "십이간지 연애유형",
         description: shareDes,
         imageUrl: shareImage,
         link: {
-            mobileWebUrl: shareURL ,
+            mobileWebUrl: shareURL,
             webUrl: shareURL,
         },
     },
