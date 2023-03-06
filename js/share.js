3b671b276b97e6682f1463c871771cfd85f81346
yuuuuun-a <124505011+yuuuuun-a@users.noreply.github.com>
2023-03-06 19:19:47 +0900

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,4 +1,4 @@
-const url = 'https://warm-crostata-a6dd43.netlify.app';
+const url = 'https://warm-crostata-a6dd43.netlify.app/';
 
 // function setShare(){
 //   var resultImg = document.querySelector('#resultImg');
@@ -32,32 +32,35 @@ const url = 'https://warm-crostata-a6dd43.netlify.app';
 //   });
 // }
 
-function setShare(){
-    var resultImg = document.querySelector('#resultImg');
-    var resultAlt = resultImg.firstElementChild.lastElementChild; // ALT 속성 지정
-    const shareTitle = '십이간지 연애유형 결과';
-    const shareDes = infoList[resultAlt].name;
-    const shareImage = url+'img/imgae-'+resultAlt+'.png';
-    const shareURL = url+'page.result-'+resultAlt+'.html';
+//!! function setShare(){
+//     var resultImg = document.querySelector('#resultImg');
+//     var resultAlt = resultImg.firstElementChild.lastElementChild; // ALT 속성 지정
+//     const shareTitle = '십이간지 연애유형 결과';
+//     const shareDes = infoList[resultAlt].name;
+//     const shareImage = "url+'img/image-'+resultAlt+'.png'";
+//     const shareURL = url+'page.result-'+resultAlt+'.html';
+//     goKakao();
+// }
 
+function goKakao(){
 Kakao.init("d3f0847972aa49b92bf926aad2ad4666");
 Kakao.Link.sendDefault({
     objectType: "feed",
     content: {
-        title: shareTitle,
-        description: shareDes,
-        imageUrl: shareImage,
+        title: "십이간지 연애유형 결과",
+        description: infoList[document.querySelector('#resultImg').firstElementChild.lastElementChild].name,
+        imageUrl: url + "img/image-" + resultImg.firstElementChild.lastElementChild + ".png"  ,
         link: {
-            mobileWebUrl: shareURL,
-            webUrl: shareURL,
+            mobileWebUrl: url + "page.result-" + resultImg.firstElementChild.lastElementChild + ".html" ,
+            webUrl: url + "page.result-" + resultImg.firstElementChild.lastElementChild + ".html",
         },
     },
     buttons: [
         {
-            title: "결과 확인하기",
+            title: "웹으로 보기",
             link: {
-                mobileWebUrl: shareURL,
-                webUrl: shareURL,
+                mobileWebUrl: url + "page.result-" + resultImg.firstElementChild.lastElementChild + ".html",
+                webUrl: url + "page.result-" + resultImg.firstElementChild.lastElementChild + ".html",
             },
         },
     ],
