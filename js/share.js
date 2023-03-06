83cc8eb464929532655b27f56a4a7ef90c3790e7
yuuuuun-a <124505011+yuuuuun-a@users.noreply.github.com>
2023-03-06 19:03:43 +0900

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,35 +1,45 @@
-const url = 'https://warm-crostata-a6dd43.netlify.app/';
+const url = 'https://warm-crostata-a6dd43.netlify.app';
+
+// function setShare(){
+//   var resultImg = document.querySelector('#resultImg');
+//   var resultAlt = resultImg.firstElementChild.alt;
+//   const shareTitle = '십이간지 연애유형 결과';
+//   const shareDes = infoList[resultAlt].name;
+//   const shareImage = url + 'img/image-' + resultAlt + '.png';
+//   const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+//   Kakao.Link.sendDefault({
+//     objectType: 'feed',
+//     content: {
+//       title: shareTitle,
+//       description: shareDes,
+//       imageUrl: shareImage,
+//       link: {
+//         mobileWebUrl: shareURL,
+//         webUrl: shareURL
+//       },
+//     },
+
+//     buttons: [
+//       {
+//         title: '결과확인하기',
+//         link: {
+//           mobileWebUrl: shareURL,
+//           webUrl: shareURL,
+//         },
+//       },
+//     ]
+//   });
+// }
 
 function setShare(){
-  var resultImg = document.querySelector('#resultImg');
-  var resultAlt = resultImg.firstElementChild.alt;
-  const shareTitle = '십이간지 연애유형 결과';
-  const shareDes = infoList[resultAlt].name;
-  const shareImage = url + 'img/image-' + resultAlt + '.png';
-  const shareURL = url + 'page/result-' + resultAlt + '.html';
-
-  Kakao.Link.sendDefault({
-    objectType: 'feed',
-    content: {
-      title: shareTitle,
-      description: shareDes,
-      imageUrl: shareImage,
-      link: {
-        mobileWebUrl: shareURL,
-        webUrl: shareURL
-      },
-    },
-
-    buttons: [
-      {
-        title: '결과확인하기',
-        link: {
-          mobileWebUrl: shareURL,
-          webUrl: shareURL,
-        },
-      },
-    ]
-  });
+    var resultImg = document.querySelector('#resultImg');
+    var resultAlt = resultImg.firstElementChild.lastElementChild; // ALT 속성 지정
+    const shareTitle = '십이간지 연애유형 결과';
+    const shareDes = infoList[resultAlt].name;
+    const shareImage = url+'img/imgae-'+resultAlt+'.png';
+    const shareURL = url+'page.result-'+resultAlt+'.html';
+    goKakao();
 }
 
 function goKakao(){
@@ -37,20 +47,20 @@ Kakao.init("d3f0847972aa49b92bf926aad2ad4666");
 Kakao.Link.sendDefault({
     objectType: "feed",
     content: {
-        title: "십이간지 연애유형",
-        description: "십이간지 연애유형 결과",
-        imageUrl: "img/share.png",
+        title: shareTitle,
+        description: shareDes,
+        imageUrl: shareImage,
         link: {
-            mobileWebUrl: "https://warm-crostata-a6dd43.netlify.app" ,
-            webUrl: "https://warm-crostata-a6dd43.netlify.app",
+            mobileWebUrl: shareURL,
+            webUrl: shareURL,
         },
     },
     buttons: [
         {
-            title: "웹으로 보기",
+            title: "결과 확인하기",
             link: {
-                mobileWebUrl: "https://warm-crostata-a6dd43.netlify.app",
-                webUrl: "https://warm-crostata-a6dd43.netlify.app",
+                mobileWebUrl: shareURL,
+                webUrl: shareURL,
             },
         },
     ],
