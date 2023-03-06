bbb4e92eaddd763fa29d77f309a9c04ae151fe5c
yuuuuun-a <124505011+yuuuuun-a@users.noreply.github.com>
2023-03-06 19:39:12 +0900

    Update share.js

--- js/share.js
+++ js/share.js
@@ -34,12 +34,12 @@ const url = 'https://warm-crostata-a6dd43.netlify.app/';
 
 
 
-    var resultImg = document.querySelector('#resultImg');
-    var resultAlt = resultImg.firstElementChild.lastElementChild; // ALT 속성 지정
-    const shareTitle = '십이간지 연애유형 결과';
-    const shareDes = infoList[resultAlt].name;
-    const shareImage = url+'img/imgae-'+resultAlt+'.png';
-    const shareURL = url+'page.result-'+resultAlt+'.html';
+    // var resultImg = document.querySelector('#resultImg');
+    // var resultAlt = resultImg.firstElementChild.lastElementChild; // ALT 속성 지정
+    // const shareTitle = '십이간지 연애유형 결과';
+    // const shareDes = infoList[resultAlt].name;
+    // const shareImage = url+'img/imgae-'+resultAlt+'.png';
+    // const shareURL = url+'page.result-'+resultAlt+'.html';
     
 
 
